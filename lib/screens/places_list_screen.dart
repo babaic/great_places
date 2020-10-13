a78091efd22416b9cbe420f9d10af9712ad75548
haris babaic <haris.babaic@outlook.com>
2020-10-13 17:19:26 +0200

    initial

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:great_places_app/providers/great_places.dart';
+import 'package:great_places_app/screens/place_detail_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'add_place_screen.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Places'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
+          )
+        ],
+      ),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: Text('Got no places yet, start adding some!'),
+                ),
+                builder: (context, greatPlaces, ch) =>
+                    greatPlaces.items.length <= 0
+                        ? ch
+                        : ListView.builder(
+                            itemCount: greatPlaces.items.length,
+                            itemBuilder: (ctx, index) => ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                    FileImage(greatPlaces.items[index].image),
+                              ),
+                              title: Text(greatPlaces.items[index].title),
+                              subtitle: Text(greatPlaces.items[index].location.address),
+                              onTap: () {
+                                Navigator.of(context).pushNamed(PlaceDetailScreen.routeName, arguments: greatPlaces.items[index].id);
+                              },
+                            ),
+                          ),
+              ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:great_places_app/helpers/location_helper.dart';
+import 'package:great_places_app/screens/map_screen.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  Function onSelectPlace;
+  LocationInput(this.onSelectPlace);
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImageUrl;
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+        latitude: locData.latitude, longitude: locData.longitude);
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+    widget.onSelectPlace(locData.latitude, locData.longitude);
+  }
+
+  Future<void> _selectOnMap() async {
+    final LatLng selectedLocation = await Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => MapScreen(
+              isSelecting: true,
+            )));
+    if(selectedLocation == null) {
+      return;
+    }
+    widget.onSelectPlace(selectedLocation.latitude, selectedLocation.longitude);
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+        latitude: selectedLocation.latitude, longitude: selectedLocation.longitude);
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+            height: 170,
+            width: double.infinity,
+            alignment: Alignment.center,
+            decoration:
+                BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
+            child: _previewImageUrl == null
+                ? Text(
+                    'No location chosen',
+                    textAlign: TextAlign.center,
+                  )
+                : Image.network(
+                    _previewImageUrl,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  )),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton.icon(
+              onPressed: _getCurrentUserLocation,
+              icon: Icon(Icons.location_on),
+              label: Text('Current Location'),
+              textColor: Theme.of(context).primaryColor,
+            ),
+            FlatButton.icon(
+              onPressed: _selectOnMap,
+              icon: Icon(Icons.map),
+              label: Text('Select on map'),
+              textColor: Theme.of(context).primaryColor,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
